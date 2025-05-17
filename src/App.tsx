509ef3176298a35ac3e43b7c37f18079cdfa1d8a
Johannes Borchard <johannesborchard@gmail.com>
2025-05-17 17:54:22 +0200

    prettier and ts config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-	return (
-		<>
-			<h1 className="text-red-400 font-bold text-5xl text-center mt-64">
-				Vite + React + TW Project
-			</h1>
-		</>
-	)
+  return (
+    <>
+      <h1 className="mt-64 text-center text-5xl font-bold text-red-400">
+        Vite + React + TW Project
+      </h1>
+    </>
+  )
 }
 
 export default App
